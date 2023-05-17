c5582b28700a247dc5ff3ab5e889b09bec18fdce
FedericoCattani <federico.cattani1@gmail.com>
2023-05-17 19:22:51 +0200

    function generaPassword

---
+++ function.php
@@ -0,0 +1,20 @@
+<?php
+
+if (isset($_GET['lunghezza'])) {
+  $lunghezza = $_GET['lunghezza'];
+  $password = generaPassowrd($lunghezza);
+}
+
+
+function generaPassowrd($lunghezza)
+{
+  $newPassword = '';
+  $charset = 'abcdefghijklmnopqrstuvwxyz0123456789!?&%$<>^+-*/()[]{}@#_=';
+  $lenght = strlen($charset);
+  for ($i = 0; $i < $lunghezza; $i++) {
+    $random = rand(1, $lenght - 1);
+    $newPassword .= $charset[$random];
+  }
+
+  return $newPassword;
+}
--- index.php
+++ index.php
@@ -1,23 +1,20 @@
 <?php
 
-//$pass = $_GET['pass'];
 
+// BUSINESS LOGIC
+include 'function.php';
 
-function generaPassword()
-{
-  $newPassRandom = rand(1, 100);
-  echo $newPassRandom;
-};
-
-
-var_dump(generaPassword());
+// 1. creare il form //[ok]
+// 2. gestiamo il submit [ok]
+// 3. prendiamo il valore inserito dall'utente // $_GET[]
+// 4. creiamo la funzione generaPassowrd[ok]
+// 4.1 creiamo una stringa vuota che conterrà la nostra password // $newPassord = '';[ok]
+// 4.2 estraiamo una lettera casuale per quanto è la lunghezza scelta dell'autente [ok]
+// 4.3 concateniamo questa nuova lettera casuale alla stringa che abbiamo creato al punto 4.1 // $newPassord .= randomCarattere;[ok]
+// 4.4 restituiamo la nuova passowrd generata [ok]
+// 5 se l'utente ha inviato il form, stampiamo la password [ok]
 
 ?>
-
-
-
-
-
 
 
 <!DOCTYPE html>
@@ -37,13 +34,19 @@ var_dump(generaPassword());
 
 <body>
   <div class="container-fc">
+    <?php if (isset($_GET['lunghezza'])) { ?>
+    <h1>La tua password generata casualmente di <?php echo $lunghezza ?> caratteri è:
+      <strong><?php echo $password ?></strong>
+    </h1>
+    <?php } else { ?>
     <form action="index.php" method="GET">
       <div class="mb-3">
-        <label for="pass" class="form-label">Password</label>
-        <input type="password" class="form-control" id="pass">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="number" class="form-control" id="exampleInputPassword1" name="lunghezza">
       </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <button type="submit" class="btn btn-primary" name="submit">Submit</button>
     </form>
+    <?php } ?>
   </div>
 </body>
 
